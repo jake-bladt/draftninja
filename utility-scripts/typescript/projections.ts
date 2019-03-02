3e941424c642a0356ebc6103acf5b4e5f23868c7
Jake Bladt <jake@jakebladt.com>
2019-03-02 17:36:58 -0500

    stats in player

--- utility-scripts/typescript/projections.ts
+++ utility-scripts/typescript/projections.ts
@@ -1,6 +1,12 @@
 declare function require(name:string);
 const fs = require('fs');
 
+class Player {
+    private _name: string;
+    get name(): string { return this._name; }
+    set name(val: string) { this._name = val; }
+}
+
 class ProjectionImporter {
 
     private _filePath: string;
@@ -13,16 +19,31 @@ class ProjectionImporter {
         return line.split('\t');
     }
 
+    private _getPlayer(line: string, headers: string[]): Player {
+        const ret: Player = new Player();
+        const stats: string[] = this._parseLine(line);
+        stats.forEach((stat, index) => {
+            const statName: string = headers[index];
+            ret[statName] = stat;
+        });
+
+        return ret;
+    }
+
     public importFromTsv() : boolean {
         fs.readFile(this._filePath, 'utf8', (err, contents) => {
             const lines: string[] = contents.split(/\r\n|\r|\n/);
-            const headers: string[] = this._parseLine(lines[0]);
+            const headers: string[] = this._parseLine(lines[1]); // Two-line header. Disregard first line.
 
-            lines.slice(1).forEach((line, index) => {
-                if(index < 50) console.log(`Line #${index}: ${line}`);
+            const body: string[] = lines.slice(2);
+            const players: Player[] = [];
+
+            body.forEach((line) => {
+                const player: Player = this._getPlayer(line, headers);
+                console.log(player["Player"]);
             });
         });
-        return false;
+        return true;
     }
 }
 
